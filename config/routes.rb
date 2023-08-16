6e2c77a8b7608924a13ed2d0f421a6bf5ecae15d
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-08-16 23:56:16 +0530

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      resources :users
+      resources :cities
+      resources :rentals
+      resources :cars
+    end
+  end
 end
