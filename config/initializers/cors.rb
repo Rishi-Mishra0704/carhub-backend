04374f10471649832fa3baab53ec4ecd3189d436
Rishi Mishra <114818980+Rishi-Mishra0704@users.noreply.github.com>
2023-08-19 02:11:05 +0530

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use Rack::Cors do
-    allow do
-      origins '*'
-      resource '*', headers: :any, methods: %i[get post options delete]
-    end
+  allow do
+    origins 'http://localhost:4000' # Adjust the frontend domain
+    resource '*', headers: :any, methods: %i[get post options delete]
   end
+end
